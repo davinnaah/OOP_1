0bb165fa2465fd3d71e3d9931e12172196a041c2
Davinna Harii <davinna.harii@emmanuel.ro>
2021-12-09 18:25:14 +0200

    new

--- OOP_1/OOP_1/ArrayHelper.cs
+++ OOP_1/OOP_1/ArrayHelper.cs
@@ -17,13 +17,48 @@ namespace OOP_1
             if (n == 1) return F;
             int i = 1;
 
-            while (F[i] <= n)
+            while (i < n-1)
             {
                 i++;
                 F[i] = F[i - 1] + F[i - 2];
 
             }
             return F;
+        }
+
+        public static int Frecventa(int[] vector, int element)
+        {
+            int frecventa = 0;
+            for (int i = 0; i < vector.Length; i++)
+            {
+                if (vector[i] == element) frecventa++;
+            }
+            return frecventa;
+        }
+        public static bool Identitate(int[,] matrix)
+        {
+            bool esteIdentitate = true;
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    if (row != col)
+                    {
+                        if (matrix[row, col] !=0)
+                        {
+                            esteIdentitate = false;
+                        }
+                    }
+                    else if (matrix[row,col] != 0)
+                    {
+                        esteIdentitate = false;
+                    }
+                }
+            }
+            if (esteIdentitate == true)
+                return true;
+            else
+                return false;
         }
     }
 }
--- OOP_1/OOP_1/Program.cs
+++ OOP_1/OOP_1/Program.cs
@@ -6,10 +6,39 @@ namespace OOP_1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Introduceti numarul de valori cerute din" + "Sirul lui Fibonacci!");
             int n;
             n = Int32.Parse(Console.ReadLine());
+            int[] vector = ArrayHelper.Fibonacci(n);
+            for (int i = 0; i < vector.Length; i++)
+                Console.WriteLine(vector[i] + " ");
 
-            int[] vector = ArrayHelper.Fibonacci(5);
+
+            Console.WriteLine("Introduceti numarul de elemente din vector:");
+            int nr = Int32.Parse(Console.ReadLine());
+            int[] vector1 = new int[nr];
+            for (int i = 0; i < nr; i++)
+            {
+                Console.Write($"vector1[{i}]");
+                vector1[i] = Int32.Parse(Console.ReadLine());
+            }
+
+
+            Console.WriteLine("Introduceti numarul de linii din matrice:");
+            int n1 = Int32.Parse(Console.ReadLine());
+            int[,] matrix = new int[n1, n1];
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    Console.Write($"matrix[{row},{col}]:");
+                    matrix[row, col] = Int32.Parse(Console.ReadLine());
+                }
+            }
+            if (ArrayHelper.Identitate(matrix) == true)
+                Console.WriteLine("Este matrice identitate!");
+            else
+                Console.WriteLine("Nu este matrice identitate!");
         }
     }
 }
